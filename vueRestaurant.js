7eae56f8f3e79a78e6e18ed69c89adf7084618d0
a.camberabero <a.camberabero@net.estia.fr>
2019-03-28 17:18:45 +0100

    Essai récupérer infos en cliquant dessus

---
+++ vueRestaurant.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#restaurant',
+    data: {
+        restaurant: [],
+        getRestaurants: 'http://localhost:8080/api/restaurants/5c63cdedc7e1817a87995a1e',
+    },
+    mounted() {
+        this.getDataFromServer(this.getRestaurants);
+    },
+    methods: {
+        getDataFromServer: function(url) {
+            var loader = document.querySelector("#loader");
+            loader.style.display = 'block';
+
+            console.log("--- GETTING DATA ---");
+            fetch(url)
+                .then(response => {
+                    return response.json(); // transforme le json texte en objet js
+                })
+                .then(data => { // data c'est l'objet ci-dessus (json devenu obj)
+                            this.restaurant = data.data;
+                    }
+                    loader.style.display = 'none';
+                    pagination.style.display = 'block';
+                }).catch(err => {
+                console.log("erreur dans le get : " + err)
+            });
+        }
+    }
+
+});
