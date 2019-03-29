3c459bfd7b67fd9e7679328a97b5cab01b9a129a
Mitsuktuk <45069279+Mitsuktuk@users.noreply.github.com>
2019-03-29 16:58:53 +0100

    recup restaurants/:id

--- vueRestaurant.js
+++ vueRestaurant.js
@@ -1,16 +1,16 @@
 var app = new Vue({
     el: '#restaurant',
     data: {
-        restaurant: [],
-        getRestaurants: 'http://localhost:8080/api/restaurants/5c63cdedc7e1817a87995a1e',
+        restaurant: null,
+        getRestaurants: 'http://localhost:8080/api/restaurants/5c63cd58d17b648636e663cc',
     },
     mounted() {
         this.getDataFromServer(this.getRestaurants);
     },
     methods: {
         getDataFromServer: function(url) {
-            var loader = document.querySelector("#loader");
-            loader.style.display = 'block';
+            //var loader = document.querySelector("#loader");
+            //loader.style.display = 'block';
 
             console.log("--- GETTING DATA ---");
             fetch(url)
@@ -18,10 +18,10 @@ var app = new Vue({
                     return response.json(); // transforme le json texte en objet js
                 })
                 .then(data => { // data c'est l'objet ci-dessus (json devenu obj)
-                            this.restaurant = data.data;
-                    }
-                    loader.style.display = 'none';
-                    pagination.style.display = 'block';
+                  this.restaurant = data.restaurant;
+                  /*loader.style.display = 'none';
+                  pagination.style.display = 'block';*/
+                  console.log(this.restaurant);
                 }).catch(err => {
                 console.log("erreur dans le get : " + err)
             });
